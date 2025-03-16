4a966c0de7fd57aaaedd23350486246cae0fa4b3
vishalnaik7 <138857779+vishalnaik7@users.noreply.github.com>
2025-03-16 21:08:08 +0530

    Update laravel.sql
    Update laravel.sql

--- laravel.sql
+++ laravel.sql
@@ -1,11 +1,11 @@
--- phpMyAdmin SQL Dump
+ -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 29, 2024 at 01:28 PM
+-- Generation Time: Mar 16, 2025 at 04:34 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.1.25
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,56 +18,32 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `laravel`
+-- Database: `mini_task_manager`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `courses`
+-- Table structure for table `cache`
 --
 
-CREATE TABLE `courses` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `instructor` varchar(255) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+CREATE TABLE `cache` (
+  `key` varchar(255) NOT NULL,
+  `value` mediumtext NOT NULL,
+  `expiration` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `courses`
---
-
-INSERT INTO `courses` (`id`, `title`, `description`, `instructor`, `created_at`, `updated_at`) VALUES
-(3, 'd', 'd555555555555555555555', 'd5555555555', '2024-10-23 05:25:28', '2024-10-23 05:25:28'),
-(4, 's', 's', 's', '2024-10-23 05:26:36', '2024-10-23 05:26:36');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `course_student`
+-- Table structure for table `cache_locks`
 --
 
-CREATE TABLE `course_student` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `course_id` bigint(20) UNSIGNED NOT NULL,
-  `student_id` bigint(20) UNSIGNED NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+CREATE TABLE `cache_locks` (
+  `key` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `expiration` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `course_student`
---
-
-INSERT INTO `course_student` (`id`, `course_id`, `student_id`, `created_at`, `updated_at`) VALUES
-(1, 3, 1, NULL, NULL),
-(2, 4, 8, NULL, NULL),
-(3, 3, 1, NULL, NULL),
-(4, 4, 1, NULL, NULL),
-(5, 3, 8, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -88,6 +64,41 @@ CREATE TABLE `failed_jobs` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `queue` varchar(255) NOT NULL,
+  `payload` longtext NOT NULL,
+  `attempts` tinyint(3) UNSIGNED NOT NULL,
+  `reserved_at` int(10) UNSIGNED DEFAULT NULL,
+  `available_at` int(10) UNSIGNED NOT NULL,
+  `created_at` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_batches`
+--
+
+CREATE TABLE `job_batches` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `total_jobs` int(11) NOT NULL,
+  `pending_jobs` int(11) NOT NULL,
+  `failed_jobs` int(11) NOT NULL,
+  `failed_job_ids` longtext NOT NULL,
+  `options` mediumtext DEFAULT NULL,
+  `cancelled_at` int(11) DEFAULT NULL,
+  `created_at` int(11) NOT NULL,
+  `finished_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -102,37 +113,17 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2024_10_09_105428_student--create', 1),
-(6, '2024_10_23_093424_create_courses_table', 2),
-(7, '2024_10_23_113409_create_notes_table', 3),
-(8, '2024_10_29_110351_create_course_student_table', 4);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `notes`
---
-
-CREATE TABLE `notes` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `course_id` bigint(20) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `file_path` varchar(255) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `notes`
---
-
-INSERT INTO `notes` (`id`, `course_id`, `title`, `file_path`, `created_at`, `updated_at`) VALUES
-(2, 4, 'wdswdse', 'notes/I7soqkFGOBYeOqYUdrDZQPJF7YsmkAjmwSsjIleq.pdf', '2024-10-23 06:08:16', '2024-10-23 06:08:16'),
-(3, 3, 'wd', 'notes/HS2dH5Oawmg0jyJiFmeuF3gW6C5sPvVvC5wW112H.pdf', '2024-10-23 06:07:42', '2024-10-23 06:07:42');
+(1, '0001_01_01_000000_create_users_table', 1),
+(2, '0001_01_01_000001_create_cache_table', 1),
+(3, '0001_01_01_000002_create_jobs_table', 1),
+(4, '2025_03_12_165339_create_tasks_table', 1),
+(5, '2025_03_12_174627_create_tasks_table', 2),
+(6, '2025_03_12_174820_create_tasks_table', 3),
+(7, '2025_03_12_175937_create_personal_access_tokens_table', 4),
+(8, '2014_10_12_100000_create_password_reset_tokens_table', 5),
+(9, '2019_08_19_000000_create_failed_jobs_table', 6),
+(10, '2019_12_14_000001_create_personal_access_tokens_table', 7),
+(11, '2025_03_13_140511_create_tasks_table', 7);
 
 -- --------------------------------------------------------
 
@@ -168,36 +159,51 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `students`
+-- Table structure for table `sessions`
 --
 
-CREATE TABLE `students` (
+CREATE TABLE `sessions` (
+  `id` varchar(255) NOT NULL,
+  `user_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `ip_address` varchar(45) DEFAULT NULL,
+  `user_agent` text DEFAULT NULL,
+  `payload` longtext NOT NULL,
+  `last_activity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+('9TNDrvLgOq6io6m4aeI1L46Rj9sHidXhVx84Yr0j', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiNE5uUjNSRTBHS0FiUWdqMHhNeU9nMTc1clRFVmplVmE4ZFhIREE1RCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1741805039),
+('PRDFyT9ul6YqW6QaZwmDtELn31LcOsVf406tGy6G', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiMGxnVjZHSDRZVURkSUliRFd5Mkp3eVlVRjdDbzM0TEJVTGt3cElTQyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1741799081);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `education` varchar(255) NOT NULL,
-  `mobile` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(222) NOT NULL,
-  `address` text NOT NULL,
-  `dob` date NOT NULL,
-  `image` varchar(255) DEFAULT NULL,
+  `title` text NOT NULL,
+  `description` text DEFAULT NULL,
+  `priority` enum('Low','Medium','High') NOT NULL,
+  `due_date` date NOT NULL,
+  `status` enum('Pending','Completed') NOT NULL DEFAULT 'Pending',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `students`
+-- Dumping data for table `tasks`
 --
 
-INSERT INTO `students` (`id`, `name`, `education`, `mobile`, `email`, `password`, `address`, `dob`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'Vishal', 'BE', '9874563210', 'vishal75naik@gmail.com', '$2y$12$Jif7NIdqK9uEWznjAJorFuNKcdnAm/X0FjXsjm5lEOnggiwoHf04y', 'Bhandup', '2024-10-16', 'images/g5F4Hz7nvYRC8lmVEsxKmn61yZUkkW6uxiq3w8kd.png', '2024-10-09 05:46:54', '2024-10-09 05:46:54'),
-(2, 'Name', 'dwd', '7715997708', 'name@gmail.com', '', 'dewd', '2024-10-08', 'images/u2JNRei6iPh9xZAuCFOR2ODc6d5rx1PwJ2BaxdYc.png', '2024-10-23 00:21:35', '2024-10-23 00:21:35'),
-(3, 'Name', 'gtg', '9821948977', 'test@gmail.comrt', '', 'rtgr', '2024-10-01', 'images/stwilnmcJyXF8L78rjKxTwyR9yZRnnFCyu3acGzj.png', '2024-10-23 00:59:41', '2024-10-23 00:59:41'),
-(4, 'Name', 'rfrf', '7715997704', 'name@gmail.comrf', '', 'rfrfr', '2024-10-22', 'images/vYpSLYYbDsABZdP1nmMBSSH2BEma2EIy4PcFapdh.png', '2024-10-23 03:46:24', '2024-10-23 03:46:24'),
-(5, 'Name', 'li', '9874563215', 'name@gmail.com555', '', '555555555', '2024-10-28', 'images/6iYJSYCtLELCNOa0VTR8ysNwEGhIiXCe2GJDbWsq.png', '2024-10-23 03:48:55', '2024-10-23 03:48:55'),
-(6, 'Name', 'OKkqAS8PQk', '7715997755', 'name@gmai5l.com', '$2y$12$Jif7NIdqK9uEWznjAJorFuNKcdnAm/X0FjXsjm5lEOnggiwoHf04y', 'gfd', '2024-10-11', 'images/nnzNzLQeDkubbC2vQMgmdWDI66QnWU6UmrdkN1PZ.png', '2024-10-23 03:58:57', '2024-10-23 03:58:57'),
-(7, 'dede', 'OKkqAS8PQk', '9821941234', 'vishal75naik@gmail.comefeww', 'OKkqAS8PQk', 'erfew', '2024-09-30', 'images/fQ00kBx5es2iAomisJLLTefpYUfT49Sv3yF4q6e0.png', '2024-10-24 05:08:03', '2024-10-24 05:08:03'),
-(8, 'test', 'HRe3dtm80v', '9874563266', 'name@gmail.comdwdwe', '$2y$12$xRQBLqoz5xm2vSdjNVjkAehcLT1jVQ/1s/qsxoumt.wudYT4maJMW', 'dwedewd', '2024-10-23', 'images/0985SCeVu7nbJxmwEwERGYg892SLp4p1tih97k8N.png', '2024-10-29 03:52:05', '2024-10-29 03:52:05');
+INSERT INTO `tasks` (`id`, `title`, `description`, `priority`, `due_date`, `status`, `created_at`, `updated_at`) VALUES
+(12, 'vishal naik', 'Test', 'Low', '2025-03-20', 'Completed', '2025-03-13 08:46:09', '2025-03-16 10:01:18'),
+(13, 'vishal naik', 'Hi i am vishal', 'Low', '2025-03-20', 'Completed', '2025-03-13 08:46:09', '2025-03-13 09:00:21'),
+(14, 'vishal naik', 'Hello', 'Medium', '2025-03-20', 'Completed', '2025-03-13 08:46:09', '2025-03-13 09:00:21');
 
 -- --------------------------------------------------------
 
@@ -217,22 +223,28 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'vishal', 'vishal7naik@gmail.com', NULL, '$2y$12$VPNE3GbPES0vZkHBpzf1QuDIbdza2K1svKLFXGhlzOdaT770uyWxq', NULL, '2025-03-14 01:27:47', '2025-03-14 01:27:47'),
+(2, 'vishal', 'vishal75naik@gmail.com', NULL, '$2y$12$rB7/N/HUEnOA0aEYwaObjOh/6lwRNmB3OAIn1fU0QimzFqFj4z/xy', NULL, '2025-03-14 01:31:38', '2025-03-14 01:31:38');
+
+--
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `courses`
+-- Indexes for table `cache`
 --
-ALTER TABLE `courses`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `cache`
+  ADD PRIMARY KEY (`key`);
 
 --
--- Indexes for table `course_student`
+-- Indexes for table `cache_locks`
 --
-ALTER TABLE `course_student`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `course_student_course_id_foreign` (`course_id`),
-  ADD KEY `course_student_student_id_foreign` (`student_id`);
+ALTER TABLE `cache_locks`
+  ADD PRIMARY KEY (`key`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -242,17 +254,23 @@ ALTER TABLE `failed_jobs`
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
 
 --
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jobs_queue_index` (`queue`);
+
+--
+-- Indexes for table `job_batches`
+--
+ALTER TABLE `job_batches`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `notes`
---
-ALTER TABLE `notes`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `notes_course_id_foreign` (`course_id`);
 
 --
 -- Indexes for table `password_reset_tokens`
@@ -269,12 +287,18 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
--- Indexes for table `students`
+-- Indexes for table `sessions`
 --
-ALTER TABLE `students`
+ALTER TABLE `sessions`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `students_mobile_unique` (`mobile`),
-  ADD UNIQUE KEY `students_email_unique` (`email`);
+  ADD KEY `sessions_user_id_index` (`user_id`),
+  ADD KEY `sessions_last_activity_index` (`last_activity`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -288,34 +312,22 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `courses`
---
-ALTER TABLE `courses`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `course_student`
---
-ALTER TABLE `course_student`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `notes`
---
-ALTER TABLE `notes`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -324,33 +336,16 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `students`
+-- AUTO_INCREMENT for table `tasks`
 --
-ALTER TABLE `students`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `tasks`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `course_student`
---
-ALTER TABLE `course_student`
-  ADD CONSTRAINT `course_student_course_id_foreign` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `course_student_student_id_foreign` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `notes`
---
-ALTER TABLE `notes`
-  ADD CONSTRAINT `notes_course_id_foreign` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
