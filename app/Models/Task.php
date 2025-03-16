20faaf101b8df55499d3f0c397d64d9c3c9e1545
vishalnaik7 <138857779+vishalnaik7@users.noreply.github.com>
2025-03-16 21:03:49 +0530

    Update Task.php

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,5 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'description', 'priority', 'due_date', 'status'];
+
+    protected $fillable = ['title', 'description', 'priority', 'due_date', 'status']; 
+    public function scopeFilter($query, $filters)
+    {
+        if (isset($filters['search'])) {
+            $query->where('title', 'like', '%' . $filters['search'] . '%');
+        }
+
+        if (isset($filters['priority'])) {
+            $query->where('priority', $filters['priority']);
+        }
+
+        if (isset($filters['status'])) {
+            $query->where('status', $filters['status']);
+        }
+
+        return $query;
+    }
 }
